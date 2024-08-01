d5c0ce1ef49b4298f8324b374e54e74186c5fc29
Andrew Alverson <aja@uark.edu>
2024-08-01 09:13:48 -0700

    script creates tree with branches colored by clade, needs node bars and timescale at bottom

--- full-tree-figure/full-figure.R
+++ full-tree-figure/full-figure.R
@@ -1,30 +1,27 @@
 library(treeio)
 library(ggtree)
 library(readxl)
-library(dplyr)
+library(tidyverse)
 library(ape)
 library(ggplot2)
 library(wesanderson)
-library(here)
 
-tree.file     <- here("data", "thal-raphid-tax0.6-pmsf.treefile")
-metadata.file <-  here("data", "tree-figs-data.xlsx")
-
-tree <- ape::read.tree(tree.file)
+# read in tree file
+diatom.tree <- treeio::read.mcmctree("data/summary-mean-brlens.nex")
 
 # read in metadata for tip labels
-metadata <- read_excel(metadata.file)
+metadata <- read_excel("data/tree-figs-data.xlsx")
 
 # root the tree
 rooted.tree <- root.phylo(
-  phy = tree,
-  outgroup = "Aureococcusanoph",
+  phy = diatom.tree@phylo,
+  outgroup = "Bolidomonas_pacifica_CCMP1866",
   resolve.root = TRUE,
   edgelabel = TRUE
 )
 
 # get vector of tips to drop
-drop.dat <- metadata |> dplyr::filter(cartoon.tree == "drop")
+drop.dat <- metadata |> dplyr::filter(keep == "drop")
 drop.vec <- drop.dat$label
 
 # drop tips
@@ -42,9 +39,15 @@ major_group_info <- split(
 major_groups <- groupOTU(rooted.tree, major_group_info)
 
 # rename tips
+metadata_tips_only <- metadata_tips_only %>%
+  unite("formatted.label", c(final.genus, final.species, strain),
+        na.rm = TRUE,
+        sep = " ")
+
+# rename tips
 major_groups.tree <- rename_taxa(
   major_groups, metadata_tips_only,
-  key = label, value = order
+  key = label, value = formatted.label
 )
 
 group_colors <- wesanderson::wes_palette(
@@ -62,21 +65,22 @@ group_colors <- c("black", "grey", "#5785c1", group_colors[-3])
 group_colors[6] <- "#D37416"
 group_colors[10] <- "#157764"
 
-
 major_group_tree_fig <-
   ggtree(
     major_groups.tree,
-    size = 0.4, aes(color = group), branch.length = "none"
-  ) +
-  ggplot2::xlim(0, 40) +
+    size = 0.4, aes(color = group)
+    ) +
+  scale_y_reverse() +
+#  ggplot2::xlim(0, 40) +
   geom_tiplab(size = 2, family = "Helvetica") +
   ggplot2::theme(legend.position = "none") +
   ggplot2::scale_color_manual(values = group_colors) +
   ggplot2::scale_fill_manual(values = group_colors)
 
-outfile = here("order-figure", "orders.pdf")
+outfile = here("full-tree-figure/full-tree.pdf")
 
 ggsave(
   major_group_tree_fig,
-  file = outfile, width = 2.75, height = 8.5
+  file = outfile, width = 6, height = 20
 )
+
