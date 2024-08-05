142753eadbe237b11db784c76752e8e3e2d0c1cb
Andrew Alverson <aja@uark.edu>
2024-08-05 13:34:02 -0500

    draws full tree with time scale and node bars

--- full-tree-figure/full-figure.R
+++ full-tree-figure/full-figure.R
@@ -9,44 +9,32 @@ library(wesanderson)
 # read in tree file
 diatom.tree <- treeio::read.mcmctree("data/summary-mean-brlens.nex")
 
-# read in metadata for tip labels
-metadata <- read_excel("data/tree-figs-data.xlsx")
-
-# root the tree
-rooted.tree <- root.phylo(
-  phy = diatom.tree@phylo,
-  outgroup = "Bolidomonas_pacifica_CCMP1866",
-  resolve.root = TRUE,
-  edgelabel = TRUE
+# drop Bolidomonas
+no_bolid <- treeio::drop.tip(
+  object = diatom.tree,
+  tip = diatom.tree@phylo$tip.label[1]
 )
+
+# read in metadata for tip labels
+diatom.data <- read_excel("data/tree-figs-data.xlsx")
 
 # get vector of tips to drop
-drop.dat <- metadata |> dplyr::filter(keep == "drop")
-drop.vec <- drop.dat$label
-
-# drop tips
-rooted.tree <- drop.tip(rooted.tree, tip = drop.vec)
-
-# copy metadata object to metadata_tips_only with only the rows matching
-# rooted.tree$tip.label, then make a list of lists from that to input to groupOTU
-metadata_tips_only <- metadata[metadata$label %in% rooted.tree$tip.label, ]
-
-### group by morphological "group" == radial, polar, araphid, or raphid
-# creates a list of lists, each one is the tips that belong to each group
-major_group_info <- split(
-  metadata_tips_only$label, metadata_tips_only$clade.color
-)
-major_groups <- groupOTU(rooted.tree, major_group_info)
+##### --- prep for plotting --- #####
+tips_only <- diatom.data[
+  diatom.data$label %in% no_bolid@phylo$tip.label,
+]
+major_group_info <- split(tips_only$label, tips_only$clade.color)
+major_groups <- groupOTU(no_bolid, major_group_info)
 
 # rename tips
-metadata_tips_only <- metadata_tips_only %>%
+tips_only <- tips_only %>%
   unite("formatted.label", c(final.genus, final.species, strain),
         na.rm = TRUE,
         sep = " ")
 
 # rename tips
-major_groups.tree <- rename_taxa(
-  major_groups, metadata_tips_only,
+major_groups@phylo <- rename_taxa(
+  major_groups@phylo, tips_only,
   key = label, value = formatted.label
 )
 
@@ -57,30 +45,35 @@ group_colors <- wesanderson::wes_palette(
 )
 group_colors <- rev(group_colors)
 group_colors <- c("black", "grey", "#5785c1", group_colors[-3])
+group_colors[6] <- "#D37416"
+group_colors[10] <- "#157764"
 
 # uncomment to see colors
 # "black"   "grey"    "#5785c1" "#5BBCD6" "#A1A376" "#F69100" "#F3A300" "#BCAA1E" "#50A45C" "#1C8E7A" "#8D473D" "#FF0000"
 
-# manually modify a few of the colors to increase contrast of adjacent colors
-group_colors[6] <- "#D37416"
-group_colors[10] <- "#157764"
-
-major_group_tree_fig <-
+p1 <-
   ggtree(
-    major_groups.tree,
+    major_groups,
     size = 0.4, aes(color = group)
     ) +
+#  geom_range("height_0.95_HPD", color = "#4361e7", size = 0.75, alpha = .7) +
   scale_y_reverse() +
-#  ggplot2::xlim(0, 40) +
+  
   geom_tiplab(size = 2, family = "Helvetica") +
-  ggplot2::theme(legend.position = "none") +
   ggplot2::scale_color_manual(values = group_colors) +
-  ggplot2::scale_fill_manual(values = group_colors)
+  ggplot2::scale_fill_manual(values = group_colors) +
+  theme_tree2() +
+  theme(legend.position = "none")
 
-outfile = here("full-tree-figure/full-tree.pdf")
+time.labels <- as.character(seq(300, 0, by=-50))
+
+p2 <- revts(p1)
+p3 <- p2 + scale_x_continuous(limits = c(-300, 200), breaks = seq(-300, 0, by=50), labels = time.labels)
+
+outfile = "full-tree-figure/full-tree.pdf"
 
 ggsave(
-  major_group_tree_fig,
-  file = outfile, width = 6, height = 20
+  p3,
+  file = outfile, width = 8, height = 20
 )
-
+ 
