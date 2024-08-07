42c0e5178d58331f1de833df5c2a4753f0375e69
Andrew Alverson <aja@uark.edu>
2024-08-07 10:18:52 -0500

    tree now has 7 focal mediophyte nodes marked

--- full-figure/full-figure.R
+++ full-figure/full-figure.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(ape)
 library(ggplot2)
 library(wesanderson)
+library(here)
 
 # read in tree file
 diatom.tree <- treeio::read.mcmctree("data/summary-mean-brlens.nex")
@@ -24,6 +25,23 @@ tips_only <- diatom.data[
 ]
 major_group_info <- split(tips_only$label, tips_only$clade.color)
 major_groups <- groupOTU(no_bolid, major_group_info)
+
+n1 <- getMRCA(phy=major_groups@phylo, 
+              c('Lithodesmium_intricatum_ECT2AJA-029_L217', 'Discostella_pseudostelligera_AJA075-4_R26'))
+n2 <- getMRCA(phy=major_groups@phylo,
+              c('Toxarium_undulatum_ECT3802_L241', 'Orthoseira_roeseana_CBG002_L503'))
+n3 <- getMRCA(phy=major_groups@phylo,
+              c('Toxarium_undulatum_ECT3802_L241', 'Lampriscus_shadboltianum_ECT2AJA-054_R71'))
+n4 <- getMRCA(phy=major_groups@phylo,
+              c('Attheya_longicornis_ECT2AJA-053_R15', 'Terpsinoe_americana_ECT2AJA-024_L208'))
+n5 <- getMRCA(phy=major_groups@phylo,
+              c('Toxarium_undulatum_ECT3802_L241', 'Papiliocellulus_elegans_CCMP3125_L103'))
+n6 <- getMRCA(phy=major_groups@phylo,
+              c('Lithodesmium_intricatum_ECT2AJA-029_L217', 'Toxarium_undulatum_ECT3802_L241'))
+n7 <- getMRCA(phy=major_groups@phylo,
+              c('Attheya_longicornis_ECT2AJA-053_R15', 'Eunotia_naegelii_FD354_L122'))
+# n8 <- getMRCA(phy=major_groups@phylo,
+#               c())
 
 # create formatted tip labels
 tips_only <- tips_only %>%
@@ -73,8 +91,31 @@ p1 <-
   
 p2 <- revts(p1)
 p3 <- p2 + scale_x_continuous(limits = c(-300, 100), breaks = seq(-300, 0, by=25), labels = time.labels)
+# thal + litho clade
+p4 <- p3 + geom_point2(aes(subset=node==n1), 
+                       color='black', size=3, shape = 19, position = position_nudge(x=-9))
+# chaetocerotales + sister clade
+p4 <- p4 + geom_point2(aes(subset=node==n2), 
+                       color='black', size=3, shape = 19, position = position_nudge(x=-7))
+# toxariales clade
+p4 <- p4 + geom_point2(aes(subset=node==n3), 
+                       color='black', size=3, shape = 19, position = position_nudge(x=-9))
+# attheya clade
+p4 <- p4 + geom_point2(aes(subset=node==n4), 
+                       color='black', size=3, shape = 19, position = position_nudge(x=-15))
+# toxariales + sister clade
+p4 <- p4 + geom_point2(aes(subset=node==n5), 
+                       color='black', size=3, shape = 19, position = position_nudge(x=-9))
+# mediophytes minus attheya clade
+p4 <- p4 + geom_point2(aes(subset=node==n6), 
+                       color='black', size=3, shape = 19, position = position_nudge(x=-8))
+# attheya plus pennates
+p4 <- p4 + geom_point2(aes(subset=node==n7), 
+                       color='black', size=3, shape = 19, position = position_nudge(x=-9))
+
 
 ggsave(
-  p3,
-  file = "full-tree-figure/full-tree.pdf", width = 8, height = 25
+  p4,
+  file = "full-figure/full-tree.pdf", width = 8, height = 25
 )
+
